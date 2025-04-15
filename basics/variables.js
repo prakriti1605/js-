735532662fe4978b38a020ad60734bc7da27e88c
prakriti1605 <122665633+prakriti1605@users.noreply.github.com>
2025-04-15 22:50:53 +0530

    Update variables.js

--- basics/variables.js
+++ basics/variables.js
@@ -1,2 +1,12 @@
 console.log("prakriti")
-
+const accountId = 3246
+let accEmail = "prakriti@123"
+var acc_pass = "jvh832jh"
+acc_city = "jaipur"
+//accountId=3 not allowed
+let acc_state
+console.log(accountId)
+console.table({accountId, accEmail, acc_pass,
+    acc_city, acc_state})
+console.table([accountId, accEmail, acc_pass,
+        acc_city, acc_state])
