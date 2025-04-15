86fd8be4a6d8ca0fa6ff2391ac1ad11810400752
prakriti1605 <122665633+prakriti1605@users.noreply.github.com>
2025-04-15 22:52:38 +0530

    Create datatype.js

---
+++ basics/datatype.js
@@ -0,0 +1,30 @@
+"use strict"; /*treat all js code as newer version, just
+a good practice */
+
+/* alert(3+3)  this is a common seen term, but we are using nodejs not browser 
+ hence this will result in error if executed as a js file */
+
+console.log(3+3)
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+/* Intro to data types
+Primitive
+
+number range =>2 ^ 53
+string=""
+boolean (true/false)
+null is a standalone value
+undefined means that value is not defined
+symbol = unique .......later
+
+derived 
+
+object
+*/
+console.log(typeof undefined); // output = undefined
+console.log(typeof null); // object
+console.log(typeof age)
+console.log(typeof state) //undefined
